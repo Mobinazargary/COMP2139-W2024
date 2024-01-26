441433382afc00d172c096257d80cd91756dab83
Mobinazr1381 <mszargary@gmail.com>
2024-01-26 18:30:35 -0500

    Your commit message

---
+++ Documents/GitProjects/COMP2129-W2024/COMP2139-Labs/COMP2139-Labs/Controllers/ProjectController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using COMP2139_Labs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace COMP2139_Labs.Models.Controllers //Controller does CRUD
+{
+    public class ProjectController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var projects = new List<Project>()
+            {
+                new Project {projectID = 1, Name = "Project 1", Description = "My first project"}
+
+            };
+
+            return View(projects);
+        }
+
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var project = new Project { projectID = id, Name = "Project " + id, Description = "Description of project " + id };
+            return View(project);
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Project project)
+        {
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
+
