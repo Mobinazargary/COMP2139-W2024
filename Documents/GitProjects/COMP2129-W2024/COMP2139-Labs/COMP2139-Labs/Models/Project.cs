7cf8c9bb2590eb5cc088c502354f98055f75fe26
Mobinazargary <156458918+Mobinazargary@users.noreply.github.com>
2024-02-19 01:20:02 -0500

    Second exercise

--- Documents/GitProjects/COMP2129-W2024/COMP2139-Labs/COMP2139-Labs/Models/Project.cs
+++ Documents/GitProjects/COMP2129-W2024/COMP2139-Labs/COMP2139-Labs/Models/Project.cs
@@ -1,32 +1,24 @@
-﻿using System;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace COMP2139_Labs.Models
 {
     public class Project
     {
-        //Attributes
-
-        [Key]
-        public int projectID { get; set; } //This creates a PRIVATE attribute but public getter and setter for the attribute
-
-        [Required] //"Required" annotation
-        public string Name { get; set; }
-
-        public string? Description { get; set; } //the question mark mean this field is nullable (optional) it can be a string or Null.
-
-        [DataType(DataType.Date)]       //This is called an annotation to enforce a certian datatype
+        public int ProjectId { get; set; }
+        [Required(ErrorMessage = "The Name field is required.")]
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
-
         [DataType(DataType.Date)]
         public DateTime EndDate { get; set; }
-
         public string? Status { get; set; }
 
-
-
-        public Project()
-        {
-        }
+        // Ensure this is a collection indicating a one-to-many relationship
+        public ICollection<ProjectTask> ProjectTasks { get; set; } = new List<ProjectTask>();
     }
+
 }
+
+
