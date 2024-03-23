e9aa93bb90113eaa0d169149bed95b6d457f0ea6
Mobinazr1381 <mszargary@gmail.com>
2024-03-23 01:51:12 -0400

    Complete third exercise

---
+++ Webapplication/COMP2139-Lab/COMP2139-Labs/COMP2139-Labs/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using COMP2139_Labs.Models;
+using Microsoft.EntityFrameworkCore;
+using COMP2139_Labs.Areas.ProjectManagement.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace COMP2139_Labs.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<ProjectTask> ProjectTasks { get; set; }
+        public DbSet<ProjectComment> ProjectComments { get; set; }
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
